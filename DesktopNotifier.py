032d011eac6a5b5f90ecec6a4591c62941c90ba7
Pearl <pearl.dhiman2004@gmail.com>
2023-07-10 20:22:53 +0530

    Used API to get new Quotes!

--- DesktopNotifier.py
+++ DesktopNotifier.py
@@ -1,8 +1,32 @@
 from plyer import notification
 import time
 import random
+import requests
+import json
+
+# A API by lukePeavey
+# https://github.com/lukePeavey/quotable
+api = "https://api.quotable.io/random"
 
 used = []
+
+def update():
+    n = len(open("quotes.txt","r").readlines())
+    if n <= 100:
+        print("Importing New Quotes......")
+        file = open("quotes.txt","a")
+        i = n
+        while i<100:
+            try:
+                req = requests.get(api)
+                if req.status_code == 200:
+                    quote = req.json()['content']
+                    print(quote)
+                    file.write(f"{quote}\n")
+                    i+=1
+            except:
+                pass
+    file.close()
 
 def alert(msg):
     notification.notify(title="Desktop Notifier",
@@ -12,7 +36,7 @@ def alert(msg):
 
 def RandomQuote():
     global used
-    file = open('quotes.txt')
+    file = open('quotes.txt',"r")
     quote = file.readlines()
     size = len(quote)
 
@@ -37,8 +61,9 @@ def Break20():
                         message="Look 20 Meter Away for 20 Seconds!",
                         timeout=20,
                         app_icon='Media\\App Icon.ico')
-    
+
 if __name__ == "__main__":
+    update()
     triggerQuote = time.ctime(time.time() + 60*random.randint(15,50))
     triggerBreak = time.ctime(time.time() + 60*20)
     print("Now:  ",time.ctime(),'\n'+"Quote:",triggerQuote,'\n'+"Break:",triggerBreak)
