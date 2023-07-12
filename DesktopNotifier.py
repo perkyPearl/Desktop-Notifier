09ec61243281e0784ddfc6e6714bd442a89d57a5
Pearl <pearl.dhiman2004@gmail.com>
2023-07-12 10:32:26 +0530

    Added Settings & Features Class

--- DesktopNotifier.py
+++ DesktopNotifier.py
@@ -4,17 +4,29 @@ import random
 import requests
 import json
 
+class Settings:
+    BreakTime = 20
+    quoteTime = random.randint(15,50)
+    WaterTime = 60
+    quoteFile = "quotes.txt"
+    AppIcon = "Media\\App Icon.ico"
+
+class Features:
+    Quotes = True
+    Break = True
+    Water_Reminder = True
+
 # A API by lukePeavey
 # https://github.com/lukePeavey/quotable
 api = "https://api.quotable.io/random"
 
 used = []
 
-def update():
-    n = len(open("quotes.txt","r").readlines())
+def fetchQuotes():
+    n = len(open(Settings.quoteFile,"r").readlines())
     if n <= 100:
         print("Importing New Quotes......")
-        file = open("quotes.txt","a")
+        file = open(Settings.quoteFile,"a")
         i = n
         while i<100:
             try:
@@ -28,15 +40,9 @@ def update():
                 pass
     file.close()
 
-def alert(msg):
-    notification.notify(title="Desktop Notifier",
-                    app_name = "Desktop",
-                    app_icon='Media//App Icon.ico',
-                    message=msg)
-
 def RandomQuote():
     global used
-    file = open('quotes.txt',"r")
+    file = open(Settings.quoteFile,"r")
     quote = file.readlines()
     size = len(quote)
 
@@ -54,28 +60,56 @@ def Quote():
     notification.notify(title='Quote of the Minute',
                         message=RandomQuote(),
                         timeout=2,
-                        app_icon='Media\\App Icon.ico')
+                        app_icon=Settings.AppIcon)
 
-def Break20():
+def Break():
     notification.notify(title='Break Time!',
                         message="Look 20 Meter Away for 20 Seconds!",
                         timeout=20,
-                        app_icon='Media\\App Icon.ico')
+                        app_icon=Settings.AppIcon)
+
+def WaterReminder():
+    notification.notify(title='Break Time!',
+                        message="Drink Water",
+                        timeout=20,
+                        app_icon=Settings.AppIcon)
+
+def Test(title,msg,appIcon):
+    notification.notify(title=title,
+                        message=msg,
+                        timeout=20,
+                        app_icon=appIcon)
 
 if __name__ == "__main__":
-    update()
-    triggerQuote = time.ctime(time.time() + 60*random.randint(15,50))
-    triggerBreak = time.ctime(time.time() + 60*20)
-    print("Now:  ",time.ctime(),'\n'+"Quote:",triggerQuote,'\n'+"Break:",triggerBreak)
+    print("Now:  ",time.ctime())
+    if Features.Quotes:
+        fetchQuotes()
+        triggerQuote = time.ctime(time.time() + 60*Settings.quoteTime)
+        print("Quote:",triggerQuote)
+    if Features.Break:
+        triggerBreak = time.ctime(time.time() + 60*Settings.BreakTime)
+        print("Break:",triggerBreak)
+    if Features.Water_Reminder:
+        triggerWater = time.ctime(time.time() + 60*Settings.WaterTime)
+        print("Water Break:",triggerWater)
     while True:
         now = time.ctime()
-        if now == triggerQuote:
-            print("Triggered Quote")
-            triggerQuote = time.ctime(time.time() + 60*20)
-            print(triggerQuote)
-            Quote()
-        if now == triggerBreak:
-            print("Triggered Break")
-            triggerBreak = time.ctime(time.time() + 60*random.randint(15,50))
-            print(triggerBreak)
-            Break20()
+
+        if Features.Water_Reminder:
+            if now == triggerWater:
+                print("Triggered Water Reminder")
+                triggerWater = time.ctime(time.time() + 60*Settings.waterTime)
+                print(triggerWater)
+                WaterReminder()
+        if Features.Quotes:
+            if now == triggerQuote:
+                print("Triggered Quote")
+                triggerQuote = time.ctime(time.time() + 60*Settings.quoteTime)
+                print(triggerQuote)
+                Quote()
+        if Features.Break:
+            if now == triggerBreak:
+                print("Triggered Break")
+                triggerBreak = time.ctime(time.time() + 60*Settings.BreakTime)
+                print(triggerBreak)
+                Break()
